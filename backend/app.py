2d1e2cb8cb6a2521b494fc1422a0989a620dfcf9
Bustiss <williejones321@gmail.com>
2025-02-18 11:17:43 -0600

    backend mongodb connection set

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# MongoDB connection string
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client.JustADB
 
 @app.route("/", methods=["GET"])
 def home():
